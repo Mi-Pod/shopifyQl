32552bdd6fa64f2d1658726724e08105ad6741ae
MipodDev <135762643+MipodDev@users.noreply.github.com>
2025-07-07 13:44:58 -0700

    graphql addition

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { shopifyQl } = require("./src/api/Shopify.QL");
 const { testConnection } = require("./src/test/connection");
+const Shop = require("./src/graphql/Shop");
 
-module.exports = { shopifyQl, testConnection };
+module.exports = { shopifyQl, testConnection, Shop };
---
+++ src/graphql/Shop.js
@@ -0,0 +1,19 @@
+import { shopifyQl } from "../api/Shopify.QL";
+
+async function getShop(site) {
+  const query = ` {
+  shop {
+    id
+    name
+  }
+}`;
+  const input = null;
+  try {
+    const response = await shopifyQl(site, query, input);
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+}
+
+module.exports = { getShop };
